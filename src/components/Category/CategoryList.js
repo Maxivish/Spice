ec4a4ca6cdd43cbf8c209b0fcc19fcb8f8f3c2bf
Maxivish <rrv170602@gmail.com>
2024-11-16 16:06:03 +0530

    first commit

---
+++ src/components/Category/CategoryList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Category.scss";
+
+const CategoryList = ({categories}) => {
+  return (
+    <div className='section-wrapper bg-whitesmoke'>
+        <div className='container'>
+          <div className='sc-title'>categories</div>
+          <section className='sc-category grid'>
+            {
+              categories.map(category => {
+                const { idCategory: id, strCategory: title, strCategoryThumb: thumbnail} = category;
+
+                return (
+                  <Link to = {`/meal/category/${title}`} className = "category-itm align-center justify-center" key = {id}>
+                    <div className='relative flex sm:align-center justify-center gap-4 mb-4'>
+                      <div className =" mx-auto p-4"><img src = {thumbnail} alt = {title} className="w-full h-auto max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl gap-4 mb-4 img_category"/></div>
+                      
+                      <div className='category-itm-title bg-orange'>
+                        <h3 className='text-white fs-11 fw-6 ls-1 text-uppercase'>{title}</h3>
+                      </div>
+                    </div>
+                  </Link>
+                )
+              })
+            }
+          </section>
+        </div>
+    </div>
+  )
+}
+
+export default CategoryList
